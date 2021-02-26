211e5252bf29179978f78fabcbe074446102e3fd
christianpaget <cjp5fd@virginia.edu>
2021-02-26 16:47:11 +0000

    added firebase

--- server.js
+++ server.js
@@ -14,6 +14,17 @@ const app = express();
 // the backend server will parse json, not a form request
 app.use(bodyParser.json());
 
+// bring in firestore
+const Firestore = require("@google-cloud/firestore");
+
+// initialize Firestore and set project id from env var
+const firestore = new Firestore(
+    {
+        projectId: process.env.GOOGLE_CLOUD_PROJECT
+    }
+);
+
+
 // mock events data - for a real solution this data should be coming 
 // from a cloud data store
 const mockEvents = {
@@ -24,12 +35,34 @@ const mockEvents = {
 };
 
 
+function getEvents(req, res) {
+    firestore.collection("Events").get()
+        .then((snapshot) => {
+            if (!snapshot.empty) {
+                const ret = { events: []};
+                snapshot.docs.forEach(element => {
+                    ret.events.push(element.data());
+                }, this);
+                console.log(ret);
+                res.json(ret);
+            } else {
+                 res.json(mockEvents);
+            }
+        })
+        .catch((err) => {
+            console.error('Error getting events', err);
+            res.json(mockEvents);
+        });
+};
 
 
 // health endpoint - returns an empty array
 app.get('/', (req, res) => {
     res.json([]);
 });
+
+
+
 
 // version endpoint to provide easy convient method to demonstrating tests pass/fail
 app.get('/version', (req, res) => {
@@ -39,14 +72,18 @@ app.get('/version', (req, res) => {
 
 // mock events endpoint. this would be replaced by a call to a datastore
 // if you went on to develop this as a real application.
-app.get('/events', (req, res) => {
+/*app.get('/events', (req, res) => {
     res.json(mockEvents);
+});*/
+
+app.get('/events', (req, res) => {
+    getEvents(req, res);
 });
 
 // Adds an event - in a real solution, this would insert into a cloud datastore.
 // Currently this simply adds an event to the mock array in memory
 // this will produce unexpected behavior in a stateless kubernetes cluster. 
-app.post('/event', (req, res) => {
+/*app.post('/event', (req, res) => {
     // create a new object from the json data and add an id
     const ev = { 
         title: req.body.title, 
@@ -57,7 +94,23 @@ app.post('/event', (req, res) => {
     mockEvents.events.push(ev);
     // return the complete array
     res.json(mockEvents);
+});*/
+
+app.post('/event', (req, res) => {
+    // create a new object from the json data and add an id
+    const ev = { 
+        title: req.body.title, 
+        description: req.body.description,
+        id : mockEvents.events.length + 1
+     }
+// this will create the Events collection if it does not exist
+    firestore.collection("Events").add(ev).then(ret => {
+        getEvents(req, res);
+    });
+
 });
+
+
 
 app.use((err, req, res, next) => {
     console.error(err.stack);
